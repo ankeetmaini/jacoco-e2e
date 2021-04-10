7b3b0e0b0b1c142952de0e97fba4308b3aaa0402
Ankeet Maini <ankeet.maini@gmail.com>
2021-04-10 15:27:32 +0530

    Remove unit tests

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -16,4 +16,9 @@ public class GreetingController {
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
+
+	@GetMapping("/hello")
+	public Greeting hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(1, "Hi and Bye");
+	}
 }
